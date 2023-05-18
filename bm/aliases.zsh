bbe0246a5ea2de0421de1fee0b9241501afade44
Admir Trakic <admir.trakic@persgroep.net>
2023-05-18 21:50:25 +0200

    Update tools incl. terraform.sh

--- bm/aliases.zsh
+++ bm/aliases.zsh
@@ -1,5 +1,16 @@
 # $HOME/.oh-my-zsh/custom/aliases.zsh
- 
+
+del_stopped(){
+	local name=$1
+	local state
+	state=$(docker inspect --format "{{.State.Running}}" "$name" 2>/dev/null)
+
+	if [[ "$state" == "false" ]]; then
+		docker rm "$name"
+	fi
+}
+alias del_stopped="del_stopped"
+
 alias g='git'
 alias gdba='git branch --no-color --merged | command grep -vE "^(\+|\*|\s*(master|develop|dev)\s*$)" | command xargs -n 1 git branch -d'
 alias tree='tree --gitignore'
@@ -7,8 +18,8 @@ alias python='python3'
 alias p='python3'
 alias t='terraform'
 alias tf='terraform'
-alias tfplan='tf plan -compact-warnings -detailed-exitcode'
 alias tfvalidate='tf validate'
 alias tffmt='tf fmt'
 alias tfinit='tf init -upgrade -reconfigure'
+alias tfplan='tf plan -compact-warnings -detailed-exitcode'
 alias tfapply='tf apply -input=false -lock=false -compact-warnings -auto-approve'
---
+++ bm/terraform.sh
@@ -0,0 +1,66 @@
+#!/usr/bin/env bash
+
+set -e
+
+TERRAFORM_VERSION=${TERRAFORM_VERSION:-"latest"}
+
+declare -a terraform=(docker run \
+  --name terraform-cli \
+  --interactive \
+  --rm \
+  --workdir /tmp/workspace/terraform \
+  --volume "$(pwd)":/tmp/workspace/terraform \
+  --volume "$(cd .. && pwd)":/tmp/workspace/work \
+  --volume "${HOME}/.edgerc:/root/.edgerc" \
+  --volume "${HOME}/.zcli/zscaler_root.pem:/usr/local/share/ca-certificates/zscaler_root.pem" \
+  --volume "${HOME}/.terraformrc:/root/.terraformrc" \
+  --volume "${HOME}/.terraform.d:/root/.terraform.d" \
+  --volume "${HOME}/.gitconfig:/root/.gitconfig" \
+  --volume "${HOME}/.aws:/root/.aws:ro" \
+  --volume "${HOME}/.ssh:/root/.ssh:ro" \
+  --env SSH_AUTH_SOCK=${SSH_AUTH_SOCK} \
+  --env TF_IN_AUTOMATION=1 \
+  --env TF_CLI_ARGS="${TF_CLI_ARGS}" \
+  --env TF_DATA_DIR="${TF_DATA_DIR}" \
+  --env TF_IGNORE="${TF_IGNORE}" \
+  --env TF_INPUT="${TF_INPUT}" \
+  --env TF_WORKSPACE="${TF_WORKSPACE}" \
+  --env TF_LOG="${TF_LOG}" \
+  --env TF_LOG_PATH="${TF_LOG_PATH}" \
+  --env TF_REGISTRY_CLIENT_TIMEOUT="${TF_REGISTRY_CLIENT_TIMEOUT}" \
+  --env TF_CLI_CONFIG_FILE="${TF_CLI_CONFIG_FILE}" \
+  --env AWS_PROFILE="${AWS_PROFILE:-'default'}" \
+  --env AWS_DEFAULT_REGION="${AWS_DEFAULT_REGION:-'eu-west-1'}" \
+  --log-driver none \
+  hashicorp/terraform:"${TERRAFORM_VERSION}"
+)
+
+main() {
+  [ ! -f "$HOME/.edgerc" ] && { echo "Error: you need edgerc file "; exit 1; }
+  command="$1"
+  shift 1
+  case "${command}" in
+      "init")
+          "${terraform[@]}" "${command}" -upgrade "${@}"
+          ;;
+      "plan")
+          "${terraform[@]}" init -upgrade
+          "${terraform[@]}" "${command}" -compact-warnings "${@}"
+          ;;
+      "apply")
+          "${terraform[@]}" init -upgrade
+          "${terraform[@]}" plan -compact-warnings
+          "${terraform[@]}" "${command}" -auto-approve "${@}"
+          ;;
+      "destroy")
+          "${terraform[@]}" init -upgrade
+          "${terraform[@]}" plan -compact-warnings
+          "${terraform[@]}" "${command}" "${@}"
+          ;;
+      *)
+          "${terraform[@]}" "${command}" "${@}"
+          ;;
+  esac
+}
+
+main "$@"
