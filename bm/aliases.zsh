c2d1b7f11260a53ce29dcbfaee968de52839f7fe
Admir Trakic <admir.trakic@persgroep.net>
2023-06-14 11:58:10 +0200

    update aliases

--- bm/aliases.zsh
+++ bm/aliases.zsh
@@ -1,16 +1,4 @@
 # $HOME/.oh-my-zsh/custom/aliases.zsh
-
-del_stopped(){
-	local name=$1
-	local state
-	state=$(docker inspect --format "{{.State.Running}}" "$name" 2>/dev/null)
-
-	if [[ "$state" == "false" ]]; then
-		docker rm "$name"
-	fi
-}
-alias del_stopped="del_stopped"
-
 alias g='git'
 alias gdba='git branch --no-color --merged | command grep -vE "^(\+|\*|\s*(master|develop|dev)\s*$)" | command xargs -n 1 git branch -d'
 alias tree='tree --gitignore'
@@ -23,3 +11,4 @@ alias tffmt='tf fmt'
 alias tfinit='tf init -upgrade -reconfigure'
 alias tfplan='tf plan -compact-warnings -detailed-exitcode'
 alias tfapply='tf apply -input=false -lock=false -compact-warnings -auto-approve'
+alias akamai-tf='AKAMAI_EDGERC_SECTION=papi akamai terraform'
